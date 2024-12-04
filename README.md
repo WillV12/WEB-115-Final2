a6ae0ca12c39573e63fef78ab1e93e0fcd431920
Will Vanderploeg <thatmurphyboi@gmail.com>
2024-12-04 18:26:38 -0500

    added all functionality
    - Fixed logic errors in the encryption/decryption logic.
    - Added some styling
    - Added comments and README

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # WEB-115-Final2
-The second portion of the solo projects for WEB 115
+- The second portion of the solo projects for WEB 115
+
+# Install
+- Download the project as a zip
+- Locate the file and extract it
+- Open the project in VsCode (make sure you have live server installed)
+    - If not, it can be downloaded in the extensions tab
+- Right click the cipher.html file and select "open with live server"
+
+# How to use
+- Encrypt:
+    - Enter any text into the text box
+    - Enter the level of shift
+    - Select Right or Left (direction of shift)
+    - Click the "go" button
+
+- Decrypt:
+    - Type or paste the encrypted text
+    - Enter the level of shift (make sure it matches the number used to encrypt)
+    - Select Right or Left (Use the same direction used to encrypt)
+    - Select decrypt
+    - Click the "go" button
+<br>
+<br>
+<img src="caption.jpg" style="height: 750px; ">
--- cipherJS.js
+++ cipherJS.js
@@ -1,33 +1,37 @@
+/*Initializing variables including the alpha array for shifting later on */
 const alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 const output = document.getElementById('output')
 const inputs = document.getElementById('cipherForm')
 
+/*Event listener on the input form that looks for a submission */
 inputs.addEventListener('submit', (event)=>{
-    event.preventDefault()
+    event.preventDefault() // stops webpage from reloading once a form is submitted
 
-    let textInput = document.getElementById('text').value.toLowerCase()
+    /*Retrieves all inputs from the form and initalizes result as an empty string */
+    let textInput = document.getElementById('text').value.toLowerCase() // retrieves and normalizes plaintext
     let shiftNum = parseInt(document.getElementById('shift').value)
     let direction = document.getElementById('direction').value
     let mode = document.getElementById('mode').value
     let result = ''
 
+    /*I check for the two possibilties that would result in a negative shift and flip the sign as needed.*/
     if ((direction == 'left' && mode == 'encrypt') || (direction == 'right' && mode =='decrypt')){
         shiftNum = shiftNum * -1
     }
+
     let letter
     let newIndex
     let wrap
-    for (let i = 0; i<textInput.length; i++){
+    for (let i = 0; i<textInput.length; i++){ // Iterates through the input string, selecting each letter 
         letter = textInput[i]
-        if (!alpha.includes(letter)){
-            result += letter
-        }
-        else{
+        
+        /*This loops finds the corresponding posistion of each letter's index in the alpha array.
+        The variable letter will be reassigned at the end of each loop*/
         for (index = 0; index<alpha.length; index++){
-            if (letter == alpha[index]){
+            if (letter == alpha[index]){ // Once the index is found, it adds the shift
                 newIndex = index+shiftNum 
-                if (newIndex > 25 || newIndex < -25){
-                    wrap = newIndex % 26
+                if (newIndex > 25 || newIndex < -25){ // checks to see if the shift will be out of range
+                    wrap = newIndex % 26 // creates a wrap variable through the use of mod
                     letter = alpha.at(wrap)
                     break
                 }
@@ -38,10 +42,10 @@ inputs.addEventListener('submit', (event)=>{
             }
             
         }
-        }
         
-        result += letter
+        
+        result += letter // appends the new text to result
     }
 
-    output.textContent = result
+    output.textContent = result // displays the result to a div made in the html file
 })
